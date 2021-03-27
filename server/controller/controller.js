a36190147b4c095c3a0e3fe14995e6acacc384f0
sahansamarasinghe001 <sahansamarasinghe001@gmail.com>
2021-03-27 23:05:38 +0000

    API web Integration

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -20,7 +20,10 @@ exports.create = (req,res)=>{
     });
 
     //save user in the database.
-    user.save(user).then(data =>{ res.send(data)}).catch(err=>{
+    user.save(user).then(data =>{ //res.send(data)
+    
+     res.redirect("/add-user");
+    }).catch(err=>{
         res.status(500).send({ message: err.message || "Some error occured" });
     });
 
@@ -30,7 +33,26 @@ exports.create = (req,res)=>{
 
 //retrieve and return all users || single user
 exports.find = (req,res)=>{
- userDB.find().then( user=>{res.send(user)}).catch(err=> res.status(500).send({message : err.message || "error in retrieving"}))
+
+    if(req.query.id){
+
+        const id = req.query.id;
+        userDB.findById(id).then(data=>{ 
+            if(!data){
+             
+                res.status(404).send({ message:"User not found"})
+            }else{
+                res.send(data)
+            }
+        }).catch(err=>{
+        res.status(500).send({ message: err.message || "Some error occured in finding User" });
+        });
+    }else{
+        
+        userDB.find().then( user=>{res.send(user)}).catch(err=> res.status(500).send({message : err.message || "error in retrieving"}))
+    }
+
+ 
 }
 
 //update user
@@ -61,5 +83,22 @@ exports.update = (req,res)=>{
 
 //delete user with specified user id
 exports.delete = (req,res)=>{
+
+    const id = req.params.id;
+    
+    userDB.findByIdAndDelete(id).then( data=>{
+
+        if(!data){
+            res.status(404).send({message :"cannot Delete user"});
+        }
+        else{
+            res.send({
+                message: "User Deleted successfully"
+            })
+        }
+
+    }).catch(err=>{
+        res.status(500).send({ message: err.message || "Some error occured in Deleting" });
+    });
 
 }
--- server/services/render.js
+++ server/services/render.js
@@ -1,5 +1,16 @@
+
+const axios = require("axios");
+
 exports.homeRoutes = (req,res) =>{
-    res.render("index");
+
+    axios.get("http://localhost:3000/api/users").then( function(response){
+        
+        res.render("index",{users: response.data});
+
+    }).catch(err=>{
+        res.send(err);
+    })
+    
 }
 exports.addUser = (req,res) =>{
     res.render("add_user");
