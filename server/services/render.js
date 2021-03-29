2349d4bdcdcd82e8a343390a094c95c6002d58f3
sahansamarasinghe001 <sahansamarasinghe001@gmail.com>
2021-03-29 16:43:04 +0100

    Final

--- server/services/render.js
+++ server/services/render.js
@@ -16,5 +16,14 @@ exports.addUser = (req,res) =>{
     res.render("add_user");
 }
 exports.updateUser = (req,res) =>{
-    res.render("update_user");
+    axios.get("http://localhost:3000/api/users/", { params :{ id : req.query.id}}).then( function(userdata){
+        
+        res.render("update_user",{user: userdata.data});
+
+    }).catch(err=>{
+        res.send(err);
+    }).catch(err=>{
+        res.send(err);
+    })
+    
 }
